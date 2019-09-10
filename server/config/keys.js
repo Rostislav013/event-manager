de777a879ade3d8037908c84b5c61f37bbfee005
Rostislav013 <46669977+Rostislav013@users.noreply.github.com>
2019-09-10 20:01:16 +0300

    small change

--- server/config/keys.js
+++ server/config/keys.js
@@ -1,5 +1,5 @@
 module.exports  = {
-    // mongoURL: 'mongodb+srv://someuser:13051991@productstutorial-p9ytu.mongodb.net/test?retryWrites=true&w=majority',
+    // mongoURL: 'mongodb+srv://someuser:********@productstutorial-p9ytu.mongodb.net/test?retryWrites=true&w=majority',
     mongoURL: 'mongodb://localhost:27017/Users',
      secretOrKey: "secret"    
  }
