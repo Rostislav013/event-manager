59b0ea625b307b88ab3591458af187b38a5160bf
Petrenko Rostislav <s1900169@edu.bc.fi>
2019-09-18 14:37:16 +0300

    date input some styles

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -3,10 +3,9 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
+
 import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
+
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 const useStyles = makeStyles(theme => ({
